bcf396a4f463c7bae5b29db29ab234764d82fbd9
yasirukularathne <yasirukularathne1234@gmail.com>
2025-09-10 21:44:50 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['html-to-text'],
-  experimental: {
-    esmExternals: 'loose'
-  }
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+    
+    config.externals = {
+      ...config.externals,
+      'html-to-text': 'html-to-text'
+    };
+    
+    return config;
+  },
+  transpilePackages: ['html-to-text']
 }
 
 module.exports = nextConfig
