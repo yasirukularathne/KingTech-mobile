6ffdec996ebed258d8936f3970c08d379cc2c81e
yasirukularathne <yasirukularathne1234@gmail.com>
2025-09-11 22:25:56 +0530

    Routeupdated

--- src/app/admin/products/[id]/download/route.ts
+++ src/app/admin/products/[id]/download/route.ts
@@ -1,7 +1,7 @@
-import db from "@/db/db"
-import { notFound } from "next/navigation"
-import { NextRequest, NextResponse } from "next/server"
-import fs from "fs/promises"
+import db from "@/db/db";
+import { notFound } from "next/navigation";
+import { NextRequest, NextResponse } from "next/server";
+import fs from "fs/promises";
 
 export async function GET(
   req: NextRequest,
@@ -10,18 +10,18 @@ export async function GET(
   const product = await db.product.findUnique({
     where: { id },
     select: { filePath: true, name: true },
-  })
+  });
 
-  if (product == null) return notFound()
+  if (product == null) return notFound();
 
-  const { size } = await fs.stat(product.filePath)
-  const file = await fs.readFile(product.filePath)
-  const extension = product.filePath.split(".").pop()
+  const { size } = await fs.stat(product.filePath);
+  const file = await fs.readFile(product.filePath);
+  const extension = product.filePath.split(".").pop();
 
   return new NextResponse(new Uint8Array(file), {
     headers: {
       "Content-Disposition": `attachment; filename="${product.name}.${extension}"`,
       "Content-Length": size.toString(),
     },
-  })
+  });
 }
